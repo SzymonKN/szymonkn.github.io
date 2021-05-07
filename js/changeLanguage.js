b71c23c9f59f21358160803dd7941f5e0fad46b4
szymonkn <szymon.skn@gmail.com>
2021-05-07 18:54:04 +0200

    Change Language Option

---
+++ js/changeLanguage.js
@@ -0,0 +1,28 @@
+function onLoad(){
+  document.getElementById("polishLanguage").addEventListener("click",function(){
+    var english = document.getElementsByClassName('englishVersion')
+
+    for (var i = 0; i < english.length; i++) {
+     english[i].style.display = 'none';
+    }
+
+    var polish = document.getElementsByClassName('polishVersion')
+
+    for (var i = 0; i < polish.length; i++) {
+     polish[i].style.display = 'block';
+    }
+  })
+  document.getElementById("englishLanguage").addEventListener("click",function(){
+    var english = document.getElementsByClassName('englishVersion')
+
+    for (var i = 0; i < english.length; i++) {
+     english[i].style.display = 'block';
+    }
+
+    var polish = document.getElementsByClassName('polishVersion')
+
+    for (var i = 0; i < polish.length; i++) {
+     polish[i].style.display = 'none';
+    }
+  })
+}
