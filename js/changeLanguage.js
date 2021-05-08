e3d860c1d18590bec970c088a92737a70a468fb7
szymonkn <szymon.skn@gmail.com>
2021-05-08 23:28:33 +0200

    auto indent lines

--- js/changeLanguage.js
+++ js/changeLanguage.js
@@ -3,26 +3,26 @@ function onLoad(){
     var english = document.getElementsByClassName('englishVersion')
 
     for (var i = 0; i < english.length; i++) {
-     english[i].style.display = 'none';
+      english[i].style.display = 'none';
     }
 
     var polish = document.getElementsByClassName('polishVersion')
 
     for (var i = 0; i < polish.length; i++) {
-     polish[i].style.display = 'block';
+      polish[i].style.display = 'block';
     }
   })
   document.getElementById("englishLanguage").addEventListener("click",function(){
     var english = document.getElementsByClassName('englishVersion')
 
     for (var i = 0; i < english.length; i++) {
-     english[i].style.display = 'block';
+      english[i].style.display = 'block';
     }
 
     var polish = document.getElementsByClassName('polishVersion')
 
     for (var i = 0; i < polish.length; i++) {
-     polish[i].style.display = 'none';
+      polish[i].style.display = 'none';
     }
   })
 
